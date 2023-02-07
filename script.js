ef8d8da620b6dd96fbab278f4594b78b702163bf
Rafael Tinti <fael145@gmail.com>
2023-02-06 22:28:25 -0300

    Adds math operations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const screen = {
     operation: operation = "",
     result: result = "0",
+    waitingNew: waitingNew = false,
 };
 
 updateScreen(screen);
@@ -13,8 +14,7 @@ btnList.forEach(button => {
 
 function updateCalc(e){
     keyId = e.target.id;
-    if (isNaN(keyId)){
-        console.log("NaN");
+    if (isNaN(keyId)){// not a numeric key
         if(keyId === "clear"){
             clearOp();
         }
@@ -25,10 +25,10 @@ function updateCalc(e){
             dotOp();
         }
         else{ // its an operation
-            operation(keyId);
+            mathOp(keyId);
         } 
     }
-    else{
+    else{ // numeric key
         numberOp();
     }
     updateScreen(screen);
@@ -46,9 +46,9 @@ function updateScreen(screen){
 
 function numberOp(){
     if (screen.result === "Infinite") return;
-    if (screen.result === "0"){
+    if (screen.result === "0" || screen.waitingNew){
         screen.result = keyId;
-        console.log("here");
+        screen.waitingNew = false;
     }
     else{
         screen.result = screen.result + keyId;
@@ -61,15 +61,76 @@ function clearOp(){
 }
 
 function deleteOp(){
+    if(screen.waitingNew) return;
     screen.result = screen.result.slice(0, screen.result.length - 1);
+    if (screen.result === ""){
+        screen.result = "0";
+    }
 }
 
 function dotOp(){
-    if(screen.result.includes(".")) return;
-    screen.result += "."
-    screen.hasDot = true;
+    if (screen.result.includes(".")) return;
+    if (screen.waitingNew){
+        screen.result = "0.";
+        screen.waitingNew = false;
+    }
+    else {
+        screen.result += "."
+    }
 }
 
-function operation(){
-    
+function mathOp(keyId){
+    if (keyId === "equals"){
+        let op = screen.operation.slice(-1)
+        if ((op === "x"||op === "+"||op === "-"||op === "/" )&&!screen.waitingNew){
+            let result = solve(op);
+            screen.operation += ` ${screen.result} =`;
+            screen.result = `${result}`;
+            screen.waitingNew = true;
+        }
+    }
+    else{ //+-*/
+        let op = screen.operation.slice(-1);
+        if (op === "x"||op === "+"||op === "-"||op === "/"){ // if the operator is at the end
+            if (screen.waitingNew){
+                screen.operation = screen.operation.slice(0, screen.operation.length - 1) + decode(keyId);
+            }
+            else{
+                let result = solve(op);
+                screen.operation = `${result} ${decode(keyId)}`;
+                screen.result = `${result}`;
+                screen.waitingNew = true;
+            }
+        }
+        else{ // if not just add the operator
+            screen.operation = screen.result + " "+ decode(keyId);
+            screen.waitingNew = true;
+        }
+    }
+}
+
+function solve(op){
+    let n1 = +screen.operation.substring(0, screen.operation.indexOf(" "));
+    let n2 = +screen.result;
+    switch (op) {
+        case "+":
+            return Math.round(1000 * (n1+n2))/1000;
+        case "-":
+            return Math.round(1000 * (n1-n2))/1000;
+        case "x":
+            return Math.round(1000 * (n1*n2))/1000;
+        case "/":
+            if (n2===0) {
+                alert("can't divide by 0");
+                return 0;
+            }
+            return Math.round(1000 * (n1/n2))/1000;
+    }
+}
+
+function decode(keyId){
+    if (keyId === "plus") return "+";
+    if (keyId === "minus") return "-";
+    if (keyId === "multiply") return "x";
+    if (keyId === "divide") return "/";
 }
